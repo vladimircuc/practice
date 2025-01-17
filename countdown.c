9fc6c1b49c4e1b18fb1352785cc9a54a0274158b
vladimircuc <vladi3030@gmail.com>
2025-01-17 13:52:27 -0500

    countdown

---
+++ countdown.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main (int argc, char *argv[])
+{
+    int nr = 10;
+
+    while(nr > 0)
+    {
+        printf("%d\n", nr);
+        nr--;
+    }
+    printf("Happy New Years!\n");
+
+    //scanf("%d", &nr);
+
+    //printf("answer: %d\n", nr > 70 && nr < 85);
+}
